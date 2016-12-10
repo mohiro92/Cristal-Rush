3aefca468279496f742872a20d87ccd6a0e5e309
Paweł <pawel.krzywodajc@gmail.com>
2016-12-10 22:41:11 +0100

    Added crystals as symbols

---
+++ Assets/Scripts/Mastermind/CrystalAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalAnimator : MonoBehaviour
+{
+    public float AnimationHeight = 0.5f;
+    public float Speed = 1.5f;
+
+    private Vector3 animationVector;
+    private Vector3 originalPosition;
+    private float animationSeed;
+
+	// Use this for initialization
+	void Start () {
+        animationVector = new Vector3(0, AnimationHeight);
+        originalPosition = transform.localPosition;
+        animationSeed = Random.Range(0, 2 * 3.1415f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = originalPosition + animationVector * Mathf.Sin(animationSeed + Speed * Time.time);
+	}
+}
