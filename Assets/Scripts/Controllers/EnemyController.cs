9842ff946961786e2e9e45bfdc113a9e815084b6
lechopl <lechopl@gmail.com>
2016-12-10 17:48:15 +0100

    Now enemy can shoot!!! :)

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Xml.Serialization;
 using Assets.Scripts;
+using Assets.Scripts.Randoms;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,12 +11,21 @@ public class EnemyController : MonoBehaviour
     public GameObject Target;
     public float NavigationTolerance = 0.5f;
 
+    public float MinShootCooldown = 100f;
+    public float MaxValueToAddShootCooldown = 50f;
+
+    private float _minShootCooldownValue = -1f;
+    private float _shootCooldownValue = 0f;
+
     private NavMeshAgent _nav;
 
     // Use this for initialization
     void Start()
     {
         _nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+        //to prevent shoot and start
+        SetNewMinCooldownValue();
     }
 
     // Update is called once per frame
@@ -23,7 +34,10 @@ public class EnemyController : MonoBehaviour
         if (CheckDead())
             return;
 
+        var deltaTime = Time.deltaTime;
+
         UpdateAgentDestination();
+        CheckShoot(deltaTime);
     }
 
     private void UpdateAgentDestination()
@@ -67,5 +81,38 @@ public class EnemyController : MonoBehaviour
     private void Kill()
     {
         gameObject.SetActive(false);
+    }
+
+    private void CheckShoot(float deltaTime)
+    {
+        _shootCooldownValue += deltaTime;
+
+        if (_shootCooldownValue > _minShootCooldownValue)
+        {
+            _shootCooldownValue = 0f;
+            SetNewMinCooldownValue();
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var entity = GetComponentInChildren<Entity>();
+        if (entity == null)
+            throw new NullReferenceException("GameObject needs Entity component");
+
+        if (Target != null)
+        {
+            var shootDir = Vector3.Normalize(Target.transform.position - transform.position);
+
+            Debug.Log("Enemy should shoot");
+            entity.Shoot(shootDir);
+        }
+    }
+
+    private void SetNewMinCooldownValue()
+    {
+        _minShootCooldownValue = MinShootCooldown + EnemyRandom.NextFloat(MaxValueToAddShootCooldown);
     }
 }
---
+++ Assets/Scripts/Randoms/EnemyRandom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assets.Scripts.Randoms
+{
+    public static class EnemyRandom
+    {
+        private static int _seed = DateTime.Now.Hour;
+        private static Random _rand;
+
+        public static Random Instance
+        {
+            get
+            {
+                if (_rand == null)
+                {
+                    _rand = new Random(Seed);
+                }
+
+                return _rand;
+            }
+        }
+
+        public static int Seed
+        {
+            get
+            {
+                return _seed;
+            }
+            set
+            {
+                _seed = value;
+
+                _rand = new Random(Seed);
+            }
+        }
+
+        public static float NextFloat(float maxValue)
+        {
+            return (float)Instance.NextDouble() *  maxValue;
+        }
+    }
+}
