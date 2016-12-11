9d835c2053d7e1db82fe75f05c74cdd899d53af5
Paweł <pawel.krzywodajc@gmail.com>
2016-12-11 04:25:16 +0100

    Added random spawns. Added buton to spawn monsters.

---
+++ Assets/Scripts/PlayerWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWrapper : MonoBehaviour {
+    public PlayerController Controller;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Controller.gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    internal void SetId(int id)
+    {
+        Controller.SetId(id);
+        gameObject.name = string.Format("Player {0}", id);
+    }
+
+    internal void Respawn(Vector3 startPosition, float deltaTime)
+    {
+        Controller.Respawn(startPosition, deltaTime);
+        gameObject.SetActive(true);
+    }
+}
